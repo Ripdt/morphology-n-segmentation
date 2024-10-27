df357c95812bdd8c67e2d0fd0f0b20623d076747
Filipi da Costa <filipiscalvindacosta@gmail.com>
2024-10-27 12:36:22 -0300

    organize main
    now it's using args to choose the project

--- main.py
+++ main.py
@@ -11,12 +11,9 @@ from os import mkdir
 from os.path import exists
 from shutil import rmtree
 
-# Criação do diretório de saída
-directory = './out'
-if exists(directory):
-    rmtree(directory)
+from argparse import ArgumentParser
 
-mkdir(directory)
+directory = './out'
 
 def show_and_save_img(img: np.ndarray, filename: str) -> None:
     cv2.imshow(filename, img)
@@ -24,32 +21,57 @@ def show_and_save_img(img: np.ndarray, filename: str) -> None:
     cv2.destroyAllWindows()
     cv2.imwrite(directory + '/' + filename + '.png', img)
 
-# ========== project 1 ==========
+def project_1() -> None:
+    img = cv2.imread('res/sfinge-fingerprint-1.jpg', cv2.IMREAD_GRAYSCALE)
+    show_and_save_img(img, 'fingerprint_original')
 
-img = cv2.imread('res/sfinge-fingerprint-1.jpg', cv2.IMREAD_GRAYSCALE)
-show_and_save_img(img, 'fingerprint_original')
+    img_basic_threshold = basic_threshold(img=img, threshold_value=100)
+    img_otsu_threshold = otsu_threshold(img=img)
 
-noise = np.random.normal(0, 50, img.shape)
-img_noise = img + noise
-show_and_save_img(img_noise, 'fingerprint_noised')
+    show_and_save_img(img_basic_threshold, 'fingerprint_basic_threshold')
+    show_and_save_img(img_otsu_threshold, 'fingerprint_otsu_threshold')
 
-img_basic_threshold = basic_threshold(img=img_noise, threshold_value=100)
-img_otsu_threshold = otsu_threshold(img=img_noise) 
+    noise = np.random.normal(0, 50, img.shape)  # Gaussian noise
+    img_noise = img + noise
+    show_and_save_img(img_noise, 'fingerprint_noised')
 
-img_basic_morphed = morph(img_basic_threshold)
-img_otsu_morphed = morph(img_otsu_threshold)
+    img_noised_basic_threshold = basic_threshold(img=img_noise, threshold_value=100)
+    img_noised_otsu_threshold = otsu_threshold(img=img_noise)
 
-show_and_save_img(img_basic_morphed, 'fingerprint_basic_threshold')
-show_and_save_img(img_otsu_morphed, 'fingerprint_otsu_threshold')
+    img_basic_morphed = morph(img_noised_basic_threshold)
+    img_otsu_morphed = morph(img_noised_otsu_threshold)
 
-# ========== project 2 ==========
+    show_and_save_img(img_basic_morphed, 'fingerprint_noised_basic_threshold')
+    show_and_save_img(img_otsu_morphed, 'fingerprint_noised_otsu_threshold')
 
-img_face = cv2.imread('res/unexistent-person-1.jpg')
-show_and_save_img(img_face, 'unexistent-person-1-original')
+def project_2() -> None:
+    img_face = cv2.imread('res/unexistent-person-1.jpg')
+    show_and_save_img(img_face, 'unexistent-person-1-original')
 
-img_skin = skin_color_thresholding(img_face)
-show_and_save_img(img_skin, 'face_skin_color_thresholding')
+    img_skin = skin_color_thresholding(img_face)
+    show_and_save_img(img_skin, 'face_skin_color_thresholding')
 
-img_kmeans = kmeans_face_detection(img=img_face, k=2)
-show_and_save_img(img_kmeans, 'face_kmeans_detection')
+    img_kmeans = kmeans_face_detection(img=img_face, k=2)
+    show_and_save_img(img_kmeans, 'face_kmeans_detection')
 
+def main() -> None:
+    parser = ArgumentParser(description='selection the desired project (\'1\', \'2\' or both)')
+    parser.add_argument('argumento', type=str, nargs='?', help='project to be executed')
+
+    args = parser.parse_args()
+
+    if exists(directory):
+        rmtree(directory)
+
+    mkdir(directory)
+
+    if args.argumento == '1':
+        project_1()
+    elif args.argumento == '2':
+        project_2()
+    else:
+        project_1()
+        project_2()
+
+if __name__ == "__main__":
+    main()
